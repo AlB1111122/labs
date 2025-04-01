c28b48c71ee4d39b834fd4c2700b0fdde62945b4
Alex <21338787@studentmail.ul.ie>
2025-04-01 13:06:21 +0100

    classify

--- l5/ll1.cc
+++ l5/ll1.cc
@@ -1,27 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<string, vector<string>> grammar;
-vector<string> nonTermOrder;
-set<string> nonTerm;
-map<string, set<string>> firstSets;
-map<string, set<string>> followSets;
-map<pair<string, string>, string> parsingTable;
-string startSymbol;
+class LL1Parser {
+ private:
+  map<string, vector<string>> grammar;
+  set<string> nonTerminals;
+  map<string, set<string>> firstSets;
+  map<string, set<string>> followSets;
+  map<pair<string, string>, string> parsingTable;
+  string startSymbol;
 
-set<string> computeFirst(string symbol) {
-  if (nonTerm.find(symbol) == nonTerm.end()) {
-    return {symbol};  // Terminal
+ public:
+  LL1Parser(const map<string, vector<string>>& inputGrammar,
+            const string& startSymbol) {
+    this->grammar = inputGrammar;
+    this->startSymbol = startSymbol;
+    for (const auto& [key, _] : grammar) {
+      nonTerminals.insert(key);
+    }
+    computeFirstSets();
+    computeFollowSets();
+    buildParsingTable();
   }
 
-  if (!firstSets[symbol].empty()) {
-    return firstSets[symbol];
-  }
+  set<string> computeFirst(const string& symbol) {
+    if (nonTerminals.find(symbol) == nonTerminals.end()) {
+      return {symbol};
+    }
 
-  for (auto &prod : grammar[symbol]) {
-    if (prod == "ε") {
-      firstSets[symbol].insert("ε");
-    } else {
+    if (!firstSets[symbol].empty()) {
+      return firstSets[symbol];
+    }
+
+    for (const auto& prod : grammar[symbol]) {
       stringstream ss(prod);
       string sym;
       vector<string> symbols;
@@ -29,7 +40,7 @@ set<string> computeFirst(string symbol) {
 
       for (size_t i = 0; i < symbols.size(); i++) {
         auto firstSym = computeFirst(symbols[i]);
-        for (auto &f : firstSym) {
+        for (const auto& f : firstSym) {
           if (f != "ε") firstSets[symbol].insert(f);
         }
         if (firstSym.find("ε") == firstSym.end()) {
@@ -40,138 +51,139 @@ set<string> computeFirst(string symbol) {
         }
       }
     }
+    return firstSets[symbol];
   }
-  return firstSets[symbol];
-}
 
-set<string> computeFollow(string symbol) {
-  if (!followSets[symbol].empty()) {
+  void computeFirstSets() {
+    for (const auto& nt : nonTerminals) {
+      computeFirst(nt);
+    }
+  }
+
+  set<string> computeFollow(const string& symbol) {
+    if (!followSets[symbol].empty()) {
+      return followSets[symbol];
+    }
+
+    if (symbol == startSymbol) {
+      followSets[symbol].insert("$");
+    }
+
+    for (const auto& [head, prods] : grammar) {
+      for (const auto& prod : prods) {
+        stringstream ss(prod);
+        string sym;
+        vector<string> symbols;
+        while (ss >> sym) symbols.push_back(sym);
+
+        for (size_t i = 0; i < symbols.size(); i++) {
+          if (symbols[i] == symbol) {
+            size_t j = i + 1;
+            while (j < symbols.size()) {
+              auto nextFirst = computeFirst(symbols[j]);
+              for (const auto& f : nextFirst) {
+                if (f != "ε") followSets[symbol].insert(f);
+              }
+              if (nextFirst.find("ε") != nextFirst.end()) {
+                j++;
+              } else {
+                break;
+              }
+            }
+            if (j == symbols.size() && head != symbol) {
+              auto followHead = computeFollow(head);
+              followSets[symbol].insert(followHead.begin(), followHead.end());
+            }
+          }
+        }
+      }
+    }
     return followSets[symbol];
   }
 
-  if (symbol == startSymbol) {
-    followSets[symbol].insert("$");
+  void computeFollowSets() {
+    for (const auto& nt : nonTerminals) {
+      computeFollow(nt);
+    }
   }
 
-  for (auto &[head, prods] : grammar) {
-    for (auto &prod : prods) {
-      stringstream ss(prod);
-      string sym;
-      vector<string> symbols;
-      while (ss >> sym) symbols.push_back(sym);
+  void buildParsingTable() {
+    for (const auto& [nonTerminal, productions] : grammar) {
+      for (const string& production : productions) {
+        stringstream ss(production);
+        string firstSymbol;
+        ss >> firstSymbol;
 
-      for (size_t i = 0; i < symbols.size(); i++) {
-        if (symbols[i] == symbol) {
-          size_t j = i + 1;
-          while (j < symbols.size()) {
-            auto nextFirst = computeFirst(symbols[j]);
-            for (auto &f : nextFirst) {
-              if (f != "ε") followSets[symbol].insert(f);
-            }
-            if (nextFirst.find("ε") != nextFirst.end()) {
-              j++;
-            } else {
-              break;
-            }
+        set<string> firstSet = computeFirst(firstSymbol);
+        for (const string& terminal : firstSet) {
+          if (terminal != "ε") {
+            parsingTable[{nonTerminal, terminal}] = production;
           }
-          if (j == symbols.size() && head != symbol) {
-            auto followHead = computeFollow(head);
-            followSets[symbol].insert(followHead.begin(), followHead.end());
+        }
+
+        if (firstSet.find("ε") != firstSet.end()) {
+          for (const string& followSymbol : computeFollow(nonTerminal)) {
+            parsingTable[{nonTerminal, followSymbol}] = production;
           }
         }
       }
     }
   }
-  return followSets[symbol];
-}
 
-void buildParsingTable() {
-  for (auto &[nonTerminal, productions] : grammar) {
-    for (string production : productions) {
-      stringstream ss(production);
-      string firstSymbol;
-      ss >> firstSymbol;
+  bool parse(const string& input) {
+    stack<string> parsingStack;
+    parsingStack.push("$");
+    parsingStack.push(startSymbol);
 
-      set<string> firstSet = computeFirst(firstSymbol);
-      for (string terminal : firstSet) {
-        if (terminal != "ε") {
-          parsingTable[{nonTerminal, terminal}] = production;
+    string modifiedInput = input + " $";
+    stringstream ss(modifiedInput);
+    vector<string> tokens;
+    string token;
+    while (ss >> token) tokens.push_back(token);
+
+    int index = 0;
+    while (!parsingStack.empty()) {
+      string top = parsingStack.top();
+      parsingStack.pop();
+
+      if (top == "$" && tokens[index] == "$") return true;
+      if (top == tokens[index]) {
+        index++;
+      } else if (parsingTable.find({top, tokens[index]}) !=
+                 parsingTable.end()) {
+        string production = parsingTable[{top, tokens[index]}];
+        stringstream ss(production);
+        vector<string> symbols;
+        while (ss >> token) symbols.push_back(token);
+        for (int i = symbols.size() - 1; i >= 0; i--) {
+          if (symbols[i] != "ε") parsingStack.push(symbols[i]);
         }
-      }
-
-      if (firstSet.find("ε") != firstSet.end()) {
-        for (string followSymbol : computeFollow(nonTerminal)) {
-          parsingTable[{nonTerminal, followSymbol}] = production;
-        }
+      } else {
+        return false;
       }
     }
+    return false;
   }
-}
-
-bool parse(string input) {
-  stack<string> parsingStack;
-  parsingStack.push("$");
-  parsingStack.push(startSymbol);
-
-  input += " $";  // end marker
-  stringstream ss(input);
-  vector<string> tokens;
-  string token;
-  while (ss >> token) tokens.push_back(token);
-
-  int index = 0;
-  while (!parsingStack.empty()) {
-    string top = parsingStack.top();
-    parsingStack.pop();
-
-    if (top == "$" && tokens[index] == "$") return true;
-
-    if (top == tokens[index]) {
-      index++;
-    } else if (parsingTable.find({top, tokens[index]}) != parsingTable.end()) {
-      string production = parsingTable[{top, tokens[index]}];
-      stringstream ss(production);
-      vector<string> symbols;
-      while (ss >> token) symbols.push_back(token);
-      for (int i = symbols.size() - 1; i >= 0; i--) {
-        if (symbols[i] != "ε") parsingStack.push(symbols[i]);
-      }
-    } else {
-      printf("Can't find: %s\n", tokens[index]);
-      return false;
-    }
-  }
-  return false;
-}
+};
 
 int main() {
-  grammar["E"] = {"T E'"};
-  grammar["E'"] = {"+ T E'", "ε"};
-  grammar["T"] = {"F T'"};
-  grammar["T'"] = {"* F T'", "ε"};
-  grammar["F"] = {"( E )", "id"};
+  map<string, vector<string>> grammar = {{"E", {"T E'"}},
+                                         {"E'", {"+ T E'", "ε"}},
+                                         {"T", {"F T'"}},
+                                         {"T'", {"* F T'", "ε"}},
+                                         {"F", {"( E )", "id"}}};
 
-  startSymbol = "E";
-
-  for (auto &[key, _] : grammar) {
-    nonTerm.insert(key);
-  }
-
-  for (auto &nt : nonTerm) computeFirst(nt);
-  for (auto &nt : nonTerm) computeFollow(nt);
-
-  buildParsingTable();
+  LL1Parser parser(grammar, grammar.begin()->first);
 
   cout << "Enter input string (space-separated tokens, e.g., 'id + id * id', "
           "represent epsilons as ε): ";
   string input;
   getline(cin, input);
 
-  if (parse(input)) {
+  if (parser.parse(input)) {
     cout << "Valid input\n";
   } else {
     cout << "Invalid input\n";
   }
-
   return 0;
 }
