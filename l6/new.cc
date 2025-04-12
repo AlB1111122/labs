74aef3553839de0a8bbed77c3a6413bdf04d4b50
Alex <21338787@studentmail.ul.ie>
2025-04-12 15:56:17 +0100

    actual parser

---
+++ l6/new.cc
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <map>
+#include <string>
+using namespace std;
+
+vector<string> inputTokens;
+map<pair<char, char>, char> precedenceTable;
+set<char> terminals;
+
+void printPrecedenceTable() {
+  cout << "\nOperator Precedence Table:\n";
+  cout << setw(10) << left << ' ';
+  for (const auto &t : terminals) cout << setw(5) << left << t;
+  cout << setw(5) << left << '$';
+  cout << "\n" << string(10 + (terminals.size() + 1) * 5, '-') << "\n";
+
+  for (const auto &row : terminals) {
+    cout << setw(10) << left << row;
+    for (const auto &col : terminals) {
+      cout << setw(5) << left << precedenceTable[{row, col}];
+    }
+    cout << setw(5) << left << precedenceTable[{row, '$'}];
+    cout << "\n";
+  }
+
+  cout << setw(10) << left << "$";
+  for (const auto &col : terminals) {
+    cout << setw(5) << left << precedenceTable[{'$', col}];
+  }
+  cout << setw(5) << left << precedenceTable[{'$', '$'}];
+  cout << "\n";
+}
+
+void fillOPT() {
+  precedenceTable[{'+', '+'}] = '>';
+  precedenceTable[{'+', '*'}] = '<';
+  precedenceTable[{'+', 'i'}] = '<';
+
+  precedenceTable[{'*', '+'}] = '>';
+  precedenceTable[{'*', '*'}] = '>';
+  precedenceTable[{'*', 'i'}] = '<';
+
+  precedenceTable[{'i', '+'}] = '>';
+  precedenceTable[{'i', '*'}] = '>';
+  precedenceTable[{'i', 'i'}] = '=';
+}
+
+void parseInputString(const string &inputString) {
+  stack<char> S;
+  string input = inputString + "$";  // Append $ to mark the end of input
+  S.push('$');                       // Start with $ symbol in the stack
+
+  cout << "\nParsing input string: " << input << endl;
+
+  int i = 0;  // Input string pointer
+
+  while (true) {
+    char top = S.top();       // Get top element from the stack
+    char current = input[i];  // Get the current character from the input string
+
+    cout << "Stack: ";
+    stack<char> temp = S;
+    while (!temp.empty()) {
+      cout << temp.top() << " ";
+      temp.pop();
+    }
+    cout << endl;
+
+    cout << "Input: " << input.substr(i) << endl;
+
+    if (top == '$' && current == '$') {
+      cout << "Parsing completed!" << endl;
+      break;
+    }
+    char precedence = precedenceTable[{top, current}];
+
+    if (precedence == '<') {
+      cout << "Shift: " << current << endl;
+      S.push(current);
+      i++;
+    } else if (precedence == '>') {
+      cout << "Reduce: " << top << endl;
+      S.pop();
+    } else if (precedence == '=') {
+      cout << "Accepting input\n";
+      break;
+    } else {
+      cout << "Error: Invalid\n" << endl;
+      break;
+    }
+  }
+}
+
+int main() {
+  string terminalsStr = "+*i";
+  cout << "Enter the terminals (single char each no spaces $ is reserved):\n";
+  //   cin >> terminalsStr;
+  if (terminalsStr.find('$') != string::npos) {
+    cout << "$ is reserved do not enter it\n";
+    return 1;
+  }
+
+  for (char &ch : terminalsStr) {
+    terminals.emplace(ch);
+  }
+
+  //   cout << "Enter the table values (>, <, or =): ";
+  //   for (char &chR : terminalsStr) {
+  //     for (char &chL : terminalsStr) {
+  //       string in;
+  //       cout << "\nEnter the table value for " << chR << " " << chL << ": ";
+  //       cin >> in;
+  //       if (in != ">" && in != "<" && in != "=") {
+  //         cout << "\nInvalid input\n";
+  //         return 1;
+  //       }
+  //       precedenceTable[{chR, chL}] = in[0];
+  //     }
+  //   }
+  fillOPT();
+
+  for (const auto &t : terminals) {
+    precedenceTable[{'$', t}] = '<';
+    precedenceTable[{t, '$'}] = '>';
+  }
+  precedenceTable[{'$', '$'}] = '=';
+
+  printPrecedenceTable();
+
+  string inputString = "i+i*i";
+  cout << "\nEnter input string: ";
+  //   cin >> inputString;
+
+  parseInputString(inputString);
+}
