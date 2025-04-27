64b13a65d6825a821f6a209bc6d6d3b812a70a38
Alex <21338787@studentmail.ul.ie>
2025-04-27 15:16:36 +0100

    more unroll

--- l8/loopUnroll.cc
+++ l8/loopUnroll.cc
@@ -9,22 +9,28 @@ void sumRegular(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     sum += arr[i];
   }
+  cout << "\nRegular sum: " << sum << endl;
 }
 
 void sumUnrolled(int arr[], int size) {
   int sum = 0;
   int i = 0;
 
-  for (; i <= size - 4; i += 4) {
+  for (; i <= size - 8; i += 8) {
     sum += arr[i];
     sum += arr[i + 1];
     sum += arr[i + 2];
     sum += arr[i + 3];
+    sum += arr[i + 4];
+    sum += arr[i + 5];
+    sum += arr[i + 6];
+    sum += arr[i + 7];
   }
 
   for (; i < size; i++) {
     sum += arr[i];
   }
+  cout << "\nUnrolled sum: " << sum << endl;
 }
 
 int main() {
@@ -36,16 +42,19 @@ int main() {
   auto start = chrono::high_resolution_clock::now();
   sumRegular(arr, SIZE);
   auto end = chrono::high_resolution_clock::now();
-  chrono::duration<double> regular_duration = end - start;
-  cout << "Time taken by regular loop: " << regular_duration.count()
-       << " seconds" << endl;
+  chrono::duration<double, milli> regular_duration = end - start;
+  cout << "Time taken by regular loop: " << regular_duration.count() << " ms"
+       << endl;
 
   start = chrono::high_resolution_clock::now();
   sumUnrolled(arr, SIZE);
   end = chrono::high_resolution_clock::now();
-  chrono::duration<double> unrolled_duration = end - start;
-  cout << "Time taken by unrolled loop: " << unrolled_duration.count()
-       << " seconds" << endl;
+  chrono::duration<double, milli> unrolled_duration = end - start;
+  cout << "Time taken by unrolled loop: " << unrolled_duration.count() << " ms"
+       << endl;
+
+  cout << "Time saved by unrolling: "
+       << regular_duration.count() - unrolled_duration.count() << " ms" << endl;
 
   return 0;
 }
